c580ae1dbbfa1d195ce4744012c60d88f4dec966
Thomas Lack <t.lack@skytron-energy.com>
2017-09-11 16:27:56 +0200

    Add link script and initial dotfiles for tmux, vim and zsh

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotfiles
 Personal preferences in various dotfiles
+
+### Usage
+After cloning the repository call `sh ./link-dotfiles.sh` from within the checkout directory. This will link all existing dotfiles into the current users home directory.
---
+++ link-dotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Creating symbolic links"
+ln -sf $DOTFILES/vim/vimrc ~/.vimrc
+ln -sf $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+ln -sf $DOTFILES/zsh ~/.zshrc
+echo "Done"
