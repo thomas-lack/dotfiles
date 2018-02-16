c437b8e959e09d50e90e0880b308a2819891a555
Thomas Lack <t.lack@skytron-energy.com>
2018-02-16 14:38:37 +0100

    Add snippet support to vim

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -3,32 +3,35 @@
 export DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PLUGINLIST=(
+    https://github.com/tpope/vim-fugitive
+    https://github.com/tpope/vim-surround
+    https://github.com/tpope/vim-repeat
+    https://github.com/scrooloose/nerdtree
+    https://github.com/Quramy/tsuquyomi
+    https://github.com/airblade/vim-gitgutter
     https://github.com/alvan/vim-closetag
     https://github.com/Raimondi/delimitMate
     https://github.com/ctrlpvim/ctrlp.vim
     https://github.com/editorconfig/editorconfig-vim
     https://github.com/morhetz/gruvbox
     https://github.com/othree/html5.vim
-    https://github.com/scrooloose/nerdtree
     https://github.com/kien/rainbow_parentheses.vim
     https://github.com/vim-syntastic/syntastic
-    https://github.com/Quramy/tsuquyomi
     https://github.com/leafgarland/typescript-vim
     https://github.com/vim-airline/vim-airline
     https://github.com/vim-airline/vim-airline-themes
     https://github.com/qpkorr/vim-bufkill
     https://github.com/flazz/vim-colorschemes
     https://github.com/ryanoasis/vim-devicons
-    https://github.com/tpope/vim-fugitive
-    https://github.com/airblade/vim-gitgutter
     https://github.com/pangloss/vim-javascript
     https://github.com/heavenshell/vim-jsdoc
-    https://github.com/tpope/vim-surround
     https://github.com/Valloric/YouCompleteMe
     https://github.com/scrooloose/nerdcommenter.git
     https://github.com/vim-IDE/MatchTagAlways
     https://github.com/luochen1990/rainbow
     https://github.com/artur-shaik/vim-javacomplete2.git
+    https://github.com/vim-scripts/SearchComplete
+    https://github.com/SirVer/ultisnips
   )
   
 # create necessaryt clone https://github.com/scrooloose/nerdcommenter.git vim folders if not available
