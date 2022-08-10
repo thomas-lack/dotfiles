f80d6a9d58b14f75db85a08fa5d78d7db770b745
Thomas Lack <post.thomas.lack@gmail.com>
2022-08-10 12:25:43 +0200

    Link custom snippets

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -5,6 +5,7 @@ export DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Creating symbolic links"
 ln -sf $DOTFILES/vim/vimrc ~/.vimrc
 ln -s $DOTFILES/.config/nvim/lua/custom ~/.config/nvim/lua/custom
+ln -s $DOTFILES/.config/snippets ~/.config/snippets
 ln -sf $DOTFILES/tmux/tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
 ln -sf $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
