484a3f9acb2f1c028fe40ad0b39ef7e34b678f2c
Thomas Lack <post.thomas.lack@gmail.com>
2022-08-26 12:52:36 +0200

    Add seperate plugin removal file

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,6 +5,7 @@ M.mappings = require "custom.mappings"
 M.plugins = {
   user = require "custom.plugins.user",
   override = require "custom.plugins.override",
+  remove = require "custom.plugins.remove",
 }
 
 return M
