d9afd8f5d33c9f6845553cab1cf421d728b9e9bc
Thomas Lack <post.thomas.lack@gmail.com>
2023-05-30 13:00:27 +0200

    Use tmux plugin manager

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -6,7 +6,7 @@ echo "Creating symbolic links"
 ln -sf $DOTFILES/vim/vimrc ~/.vimrc
 ln -s $DOTFILES/.config/nvim/lua/custom ~/.config/nvim/lua/custom
 ln -s $DOTFILES/.config/snippets ~/.config/snippets
-ln -sf $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
 ln -sf $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 echo "Done"
