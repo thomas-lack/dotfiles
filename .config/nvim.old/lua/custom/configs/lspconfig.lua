c12a6cfa11b83503bcb2079da558d01c8e500ab3
Thomas Lack <post.thomas.lack@gmail.com>
2024-08-13 13:47:10 +0200

    Move chadrc neovim config

---
+++ .config/nvim.old/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,57 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = {
+  "angularls",
+  "cssls",
+  "html",
+  "svelte",
+  "elmls",
+  -- "jdtls",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig["tsserver"].setup {
+  on_attach = on_attach,
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx","svelte" },
+  importModuleSpecifierPreference = "relative",
+  init_options = {
+    documentFormatting = true,
+    preferences = {
+      importModuleSpecifierPreference = "relative",
+    },
+  },
+  settings = {
+    rootMarkers = { ".git/" },
+    languages = {
+      prettier = {
+        formatCommand = 'prettierd "${INPUT}"',
+        formatStdin = true,
+      },
+    },
+  },
+}
+-- efm setup example
+-- lspconfig["efm"].setup {
+--   on_attach = on_attach,
+--   filetypes = { 'ts', 'js', 'scss', 'html', 'svelte' },
+--   init_options = {documentFormatting = true},
+--   settings = {
+--       rootMarkers = {".git/"},
+--       languages = {
+--           prettier = {
+--             formatCommand = 'prettierd "${INPUT}"',
+--             formatStdin = true,
+--           }
+--       }
+--   },
+-- }
---
+++ .config/nvim.old/lua/custom/highlights.lua
@@ -0,0 +1,60 @@
+-- To find any highlight groups: "<cmd> Telescope highlights"
+-- Each highlight group can take a table with variables fg, bg, bold, italic, etc
+-- base30 variable names can also be used as colors
+
+local M = {}
+
+---@type Base46HLGroupsList
+M.override = {
+  Comment = {
+    italic = true,
+  },
+  [ "@variable" ] = { italic = true },
+  -- imports
+  Include = {
+    italic = true,
+  },
+  -- for, while loops
+  Repeat = {
+    italic = true,
+  },
+  -- if statements
+  Conditional = {
+    italic = true,
+  },
+  -- class, with, as keywords
+  ["@keyword"] = {
+    italic = true,
+  },
+  -- function keyword
+  ["@keyword.function"] = {
+    italic = true,
+  },
+  -- return, yield keywords
+  ["@keyword.return"] = {
+    bold = true,
+  },
+  -- function names
+  ["@function"] = {
+    italic = false,
+  },
+  -- symbolic
+  ["@operator"] = {
+    bold = true,
+  },
+  -- logical keywords 
+  ["@keyword.operator"] = {
+    italic = true,
+  },
+  -- function parameters
+  ["@parameter"] = {
+    italic = true,
+  },
+}
+
+---@type HLTable
+M.add = {
+  NvimTreeOpenedFolderName = { fg = "green", bold = true },
+}
+
+return M
