a3a1ee1c9202dc87e9ab2798f162bcf56ccb3bd8
Thomas Lack <post.thomas.lack@gmail.com>
2024-09-28 10:58:18 +0200

    remove libusb

--- nixos/home/thomas/programs/usb.nix
+++ nixos/home/thomas/programs/usb.nix
@@ -1,12 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   services.udiskie = {
     enable = true;
     automount = true;
     notify = true;
     tray = "auto";
   };
-
-  home.packages = with pkgs; [
-    libusb
-  ];
 }
