58f97061595eb8843d3e6f5096dfcdf7b21715e1
Thomas Lack <post.thomas.lack@gmail.com>
2024-12-01 22:59:43 +0100

    update to 24.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,13 +2,13 @@
   description = "nixderp NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
