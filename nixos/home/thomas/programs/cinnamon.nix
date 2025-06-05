1845fdadb12bff5f734d9faeeecfaf7e4d863abd
Thomas Lack <post.thomas.lack@gmail.com>
2025-06-05 22:36:07 +0200

    update

--- nixos/home/thomas/programs/cinnamon.nix
+++ nixos/home/thomas/programs/cinnamon.nix
@@ -3,22 +3,23 @@
     ulauncher
   ];
 
-  systemd.user.services = {
-    ulauncher = {
-      Unit = {
-        Description = "Linux Application Launcher";
-        Documentation = "https://ulauncher.io";
-      };
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-      Service = {
-        ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
-        Environment = ["GDK_BACKEND=x11"];
-        Type = "simple";
-        Restart = "always";
-        RestartSec = 1;
-      };
+  systemd.user.services.ulauncher = {
+    Unit = {
+      Description = "Linux Application Launcher";
+      Documentation = "https://ulauncher.io";
+    };
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+    Service = {
+      ExecStart = pkgs.writeShellScript "ulauncher-env-wrapper.sh" ''
+        export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+        export GDK_BACKEND=x11
+        exec ${pkgs.ulauncher}/bin/ulauncher --hide-window
+      '';
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 1;
     };
   };
 }
