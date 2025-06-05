ffeeb0cffff897638f9214ef9a2cff4039273a13
Thomas Lack <post.thomas.lack@gmail.com>
2025-06-05 16:22:08 +0200

    update

--- nixos/home/thomas/programs/cinnamon.nix
+++ nixos/home/thomas/programs/cinnamon.nix
@@ -2,4 +2,23 @@
   home.packages = with pkgs; [
     ulauncher
   ];
+
+  systemd.user.services = {
+    ulauncher = {
+      Unit = {
+        Description = "Linux Application Launcher";
+        Documentation = "https://ulauncher.io";
+      };
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+      Service = {
+        ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
+        Environment = ["GDK_BACKEND=x11"];
+        Type = "simple";
+        Restart = "always";
+        RestartSec = 1;
+      };
+    };
+  };
 }
