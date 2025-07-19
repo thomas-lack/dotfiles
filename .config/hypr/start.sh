5a885f0be29cb3dedccfbd1990aeae068a775395
Thomas Lack <post.thomas.lack@gmail.com>
2025-07-19 23:58:05 +0200

    add hyprland and waybar config

---
+++ .config/hypr/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# initialize wallpaper daemon
+#swww init &
+# set wallpaper
+#swww img ~/path/to/file.png &
+
+# networking
+#nm-applet --indicator &
+
+killall waybar
+waybar &
+#dunst
