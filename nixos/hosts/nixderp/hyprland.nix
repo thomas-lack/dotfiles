7223ba4ff08999b9d5e84735ca1af011971c57f5
Thomas Lack <post.thomas.lack@gmail.com>
2025-07-20 21:31:08 +0200

    update

--- nixos/hosts/nixderp/hyprland.nix
+++ nixos/hosts/nixderp/hyprland.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
   };
 
@@ -8,4 +9,12 @@
     waybar
     hyprpaper
   ];
+
+  # enable screen sharing
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+    ];
+  };
 }
