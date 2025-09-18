ff5c2f7f53b5d375422e960cc53091f9a2628eb6
Thomas Lack <post.thomas.lack@gmail.com>
2025-09-18 10:03:35 +0200

    update

--- nixos/home/shared/shell/default.nix
+++ nixos/home/shared/shell/default.nix
@@ -28,7 +28,7 @@
   # add aliases
   home.shellAliases = {
     vim = "nvim";
-    code = "LD_LIBRARY_PATH=$(nix-build '<nixpkgs>' -A stdenv.cc.cc.lib)/lib code";
+    code = "LD_LIBRARY_PATH=$(nix-build '<nixpkgs>' -A stdenv.cc.cc.lib)/lib code --password-store=\"gnome-libsecret\"";
     y = "yazi";
     n = "cd /etc/nixos";
     d = "cd $HOME/Dropbox/Dokumente";
