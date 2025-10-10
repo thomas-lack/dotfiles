d3d13ca185552751366b0f3f5e753cc62739ffc5
Thomas Lack <post.thomas.lack@gmail.com>
2025-10-10 09:35:55 +0200

    update

--- nixos/home/shared/shell/zellij.nix
+++ nixos/home/shared/shell/zellij.nix
@@ -1,7 +1,7 @@
 {...}: {
   programs.zellij = {
     enable = true;
-    enableZshIntegration = true;
+    enableZshIntegration = false;
     settings = {
       default_mode = "normal";
       default_layout = "default";
