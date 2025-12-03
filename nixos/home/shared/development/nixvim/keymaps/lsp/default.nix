d107f60fb23eb2ad1920ad57439b0a8e6257c27f
Thomas Lack <post.thomas.lack@gmail.com>
2025-12-03 08:18:06 +0100

    update

--- nixos/home/shared/development/nixvim/keymaps/lsp/default.nix
+++ nixos/home/shared/development/nixvim/keymaps/lsp/default.nix
@@ -17,4 +17,40 @@
       silent = true;
     };
   }
+  {
+    mode = "n";
+    key = "gy";
+    action = "<cmd>Lspsaga goto_type_definition<cr>";
+    options = {
+      desc = "[g]o to t[y]pe definition with LSP";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "gi";
+    action = "<cmd>Lspsaga finder imp<cr>";
+    options = {
+      desc = "[g]o to [i]mplementation with LSP";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>lff";
+    action = "<cmd>Lspsaga finder<cr>";
+    options = {
+      desc = "[f]ind with LSP";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>r";
+    action = "<cmd>Lspsaga rename<cr>";
+    options = {
+      desc = "[r]ename with LSP";
+      silent = true;
+    };
+  }
 ]
