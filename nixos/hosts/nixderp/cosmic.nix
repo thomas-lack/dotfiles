136dbf4ad17cba6eed9a9a7b2a135dcd0306b622
Thomas Lack <post.thomas.lack@gmail.com>
2025-12-22 11:19:06 +0100

    update

--- nixos/hosts/nixderp/cosmic.nix
+++ nixos/hosts/nixderp/cosmic.nix
@@ -1,6 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   services = {
     desktopManager.cosmic.enable = true;
     system76-scheduler.enable = true;
   };
+
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-edit
+    cosmic-files
+  ];
+
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 }
