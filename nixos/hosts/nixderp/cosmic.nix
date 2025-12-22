0438613f83fd44d3762f4314e4ed3d65c7536560
Thomas Lack <post.thomas.lack@gmail.com>
2025-12-22 11:21:14 +0100

    update

--- nixos/hosts/nixderp/cosmic.nix
+++ nixos/hosts/nixderp/cosmic.nix
@@ -4,10 +4,10 @@
     system76-scheduler.enable = true;
   };
 
-  environment.cosmic.excludePackages = with pkgs; [
-    cosmic-edit
-    cosmic-files
-  ];
+  #environment.cosmic.excludePackages = with pkgs; [
+  #  cosmic-edit
+  #  cosmic-files
+  #];
 
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 }
