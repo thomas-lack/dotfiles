bae50f00912ef2b0adc4c233d332ef6834565977
Thomas Lack <post.thomas.lack@gmail.com>
2026-01-27 19:50:14 +0100

    update

--- nixos/home/thomas/programs/wine.nix
+++ nixos/home/thomas/programs/wine.nix
@@ -1,12 +1,11 @@
 {unstablePkgs, ...}: {
   home.packages = with unstablePkgs; [
     #wineWowPackages.unstable
-    wineWowPackages.stagingFull.override
-    {
+    (wineWowPackages.stagingFull.override {
       waylandSupport = true;
       vulkanSupport = true;
       x11Support = false;
-    }
+    })
     winetricks
   ];
 }
