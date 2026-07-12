aebd13a704ea3443d3f01ff43a032f0ba0d1ec7b
Thomas Lack <post.thomas.lack@gmail.com>
2026-07-12 13:37:33 +0200

    update

--- nixos/home/thomas/programs/gaming.nix
+++ nixos/home/thomas/programs/gaming.nix
@@ -5,11 +5,12 @@
     #/nix/store/s74admfd3w6nwf801qdf804wkxvmamcj-lutris-0.5.22
     vice
     #/nix/store/7b20bhwighgigvwq40wdlkdbzkgda5p7-vice-3.10
-    (bottles.override {
-      removeWarningPopup = true;
-    })
+    #(bottles.override {
+    #  removeWarningPopup = true;
+    #})
     # Directly use the pre-built Bottles package until openldap dependency is working
-    #/nix/store/8b6amkky52ivhjxq62vffvyaqhifsghc-bottles-63.2
+    /nix/store/8b6amkky52ivhjxq62vffvyaqhifsghc-bottles-63.2
+    /nix/store/nnawqcd49f8nb7kqm72jcxay588bd8rj-nixos-render-docs-0.0
     protonup-rs
   ];
 }
