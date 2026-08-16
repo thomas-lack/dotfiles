644fb97fe5fad816eef12686e5501ec349e1cea5
Thomas Lack <post.thomas.lack@gmail.com>
2026-08-16 12:15:48 +0200

    update

--- nixos/home/thomas/programs/gaming.nix
+++ nixos/home/thomas/programs/gaming.nix
@@ -3,13 +3,13 @@
     lutris
     # Directly use the pre-built Lutris package until openldap dependency is working
     #/nix/store/s74admfd3w6nwf801qdf804wkxvmamcj-lutris-0.5.22
-    #vice
+    vice
     #/nix/store/7b20bhwighgigvwq40wdlkdbzkgda5p7-vice-3.10
-    #(bottles.override {
-    #  removeWarningPopup = true;
-    #})
+    (bottles.override {
+      removeWarningPopup = true;
+    })
     # Directly use the pre-built Bottles package until openldap dependency is working
-    /nix/store/8b6amkky52ivhjxq62vffvyaqhifsghc-bottles-63.2
+    #/nix/store/8b6amkky52ivhjxq62vffvyaqhifsghc-bottles-63.2
     protonup-rs
   ];
 }
